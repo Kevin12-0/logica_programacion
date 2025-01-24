dc33adb5bb1a05ef6fd6c67c7e87b08da32874e2
Kevin12-0 <1719110155@utectulancingo.edu.mx>
2025-01-24 14:04:44 -0600

    cadena template string

---
+++ app.js
@@ -0,0 +1,18 @@
+/* alert de hola mundo */
+alert("Hola Mundo");
+/* insertar un promp pedir un numero 
+declaracion de variable*/
+let secretNumber = 6;
+let numberUser = prompt(
+  "Me indicas un numero por favor entre 1 - 10 por favor"
+);
+/* verificar si se guardo la varibale */
+alert(numberUser);
+/* comparacion obligatoria */
+if (numberUser == secretNumber) {
+  alert(
+    `es igual, el numero es: ${numberUser} `
+  ); /* cadena con template string */
+} else {
+  alert("no es igual");
+}
