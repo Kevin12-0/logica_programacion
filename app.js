69fda083705b21205439c88962ec59555a415fc0
Kevin12-0 <1719110155@utectulancingo.edu.mx>
2025-01-24 16:57:49 -0600

    loop de oportunidades

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ alert("Hola Mundo");
 /* insertar un promp pedir un numero 
 declaracion de variable*/
 let secretNumber = 6;
-let numberUser = prompt(
-  "Me indicas un numero por favor entre 1 - 10 por favor"
-);
-/* verificar si se guardo la varibale */
-alert(numberUser);
-/* comparacion obligatoria */
-if (numberUser == secretNumber) {
-  alert(
-    `es igual, el numero es: ${numberUser} `
-  ); /* cadena con template string */
-} else {
-  /* condicional anidado */
-  if (numberUser > secretNumber) {
-    alert("el numero es menor");
+let numberUser = 0;
+/* loop oportunidades */
+while (numberUser != secretNumber) {
+  let numberUser = prompt(
+    "Me indicas un numero por favor entre 1 - 10 por favor"
+  );
+  /* verificar si se guardo la varibale */
+  alert(numberUser);
+  /* comparacion obligatoria */
+  if (numberUser == secretNumber) {
+    alert(
+      `es igual, el numero es: ${numberUser} `
+    ); /* cadena con template string */
   } else {
-    alert("el numero en mayor");
+    /* condicional anidado */
+    if (numberUser > secretNumber) {
+      alert("el numero es menor");
+    } else {
+      alert("el numero en mayor");
+    }
+    alert("no es igual");
   }
-  alert("no es igual");
 }
